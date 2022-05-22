85b6b38ffe0d93df4e8b5dd291ebe8cbc3120d39
gs97ahn <gs97ahn@naver.com>
2022-05-22 15:08:21 +0900

    greedy/1주차/3-3.py

--- 안검성/Greedy/3-3.py
+++ 안검성/Greedy/3-3.py
@@ -1,13 +1,10 @@
 N, M = map(int, input().split())
 
 data = []
-temp = []
+min_data = []
 for i in range(N):
     temp = list(map(int, input().split()))
     data.append(temp)
-
-min_data = []
-for j in range(N):
-    min_data.append(min(data[j]))
+    min_data.append(min(temp))
 
 print(max(min_data))
