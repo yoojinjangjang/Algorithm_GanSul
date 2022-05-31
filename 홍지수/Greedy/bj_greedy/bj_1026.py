166a17b05b93df3d795db421648dafcae93440e0
g2da <hon9g2@gmail.com>
2022-05-31 13:46:33 +0900

    greedy/2주차/1026.py

---
+++ 홍지수/Greedy/bj_greedy/bj_1026.py
@@ -0,0 +1,14 @@
+'''
+ 보물 - [boj] 1026
+'''
+import sys
+
+N = int(sys.stdin.readline())
+A = sorted(list(map(int, sys.stdin.readline().split())), reverse=True)
+B = sorted(list(map(int, sys.stdin.readline().split())))
+cnt = 0
+
+for i in range(N):
+    cnt += A[i] * B[i]
+
+print(cnt)
