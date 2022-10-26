0de7ae6dfd175eb3d4c07c83bc818011c479513c
wxxhyeong <wxxhyeong@gmail.com>
2022-10-26 19:35:25 +0900

    temp

---
+++ 조우형/DFS_BFS/Q19.py
@@ -0,0 +1,23 @@
+'''
+DFS/BFS 기출 문제 19. 연산자 끼워 넣기
+
+Ex)
+n : 2
+수열 : 5 6
+연산자 종류(+, -, x, /) : 0 0 1 0
+'''
+n = int(input())
+A = list(map(int, input().split()))
+op = list(map(int, input().split()))
+res = 0
+cnt = 0
+
+def dfs(a, idx, cnt):
+
+
+
+    if idx == 0 and op[]:
+        res = a +
+
+
+
