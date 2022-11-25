e6c3ae1f74bd9ebe3dab17ccf87db25d99013805
wxxhyeong <wxxhyeong@gmail.com>
2022-11-25 19:30:30 +0900

    Binary_Search/11월23일/Q28/임시

--- 조우형/Binary_Search/boj2467.py
+++ 조우형/Binary_Search/boj2467.py
@@ -9,13 +9,57 @@ list = [-99, -2, -1, 4, 98]
 '''
 n = int(input())
 array = list(map(int, input().split()))
+array.sort()
 
-sort(array)
-start = 0
-end = len(sort) - 1
-
-result = 1e9
+result = abs(array[-2] + array[-1])
 ans = []
+
+for i in range(len(array)):
+    start = i+1
+    end = len(array)-1
+
+    while start >= end:
+        mid = (start + end)//2
+
+        total = abs(array[i]+array[mid])
+        if total < result:
+            total = result
+            ans[0] = array[i]
+            ans[1] = array[mid]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for i in range(len(array)):
+    if i == len(array):
+        break
+
+    mid = ((i+1)+(len(array)-1))//2
+    temp = abs(array[i] + array[mid])
+
+    if temp < result:
+        ans[0] = i
+        ans[1] = mid
+
+
+
+
+
+
+
+
+
 while start <= end:
     sum = 0
     mid = (start + end)//2
