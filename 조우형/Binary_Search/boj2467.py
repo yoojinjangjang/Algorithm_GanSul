0a381280bfdcb7e0789f3a18782238a621ceb1f4
wxxhyeong <wxxhyeong@gmail.com>
2022-11-27 18:16:12 +0900

    Binary_Search/11월27일/boj2467.py(용액)

--- 조우형/Binary_Search/boj2467.py
+++ 조우형/Binary_Search/boj2467.py
@@ -11,62 +11,20 @@ n = int(input())
 array = list(map(int, input().split()))
 array.sort()
 
-result = abs(array[-2] + array[-1])
-ans = []
+start = 0
+end = n - 1
+max_Num = 2e9 + 1
+result = []
 
-for i in range(len(array)):
-    start = i+1
-    end = len(array)-1
+while start < end:
+    total = array[start] + array[end]
+    if abs(total) < max_Num:
+        max_Num = abs(total)
+        result = [array[start], array[end]]
 
-    while start >= end:
-        mid = (start + end)//2
+    if total < 0:
+        start = start + 1
+    else:
+        end = end-1
 
-        total = abs(array[i]+array[mid])
-        if total < result:
-            total = result
-            ans[0] = array[i]
-            ans[1] = array[mid]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for i in range(len(array)):
-    if i == len(array):
-        break
-
-    mid = ((i+1)+(len(array)-1))//2
-    temp = abs(array[i] + array[mid])
-
-    if temp < result:
-        ans[0] = i
-        ans[1] = mid
-
-
-
-
-
-
-
-
-
-while start <= end:
-    sum = 0
-    mid = (start + end)//2
-
-    if abs(array[start]+array[end]) < result:
-        ans[0] = array[start]
-        ans[1] = array[end]
-
-
-
+print("{} {}".format(result[0], result[1]))
