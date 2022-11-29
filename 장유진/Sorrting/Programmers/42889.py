406d8c21872b144bb18c77c1d1027aaedb3198d6
yoojin Jang <yoojinjangjang@gmail.com>
2022-11-30 07:55:56 +0900

    Sort/11월30일/P_42889.py

---
+++ 장유진/Sorrting/Programmers/42889.py
@@ -0,0 +1,19 @@
+def solution(N, stages):
+    answer = []
+    stage_num_to_fail = {}
+    total_num = len(stages)
+    for i in range(1,N+1):
+        stage_fail_num = stages.count(i)
+        if stage_fail_num == 0:
+            stage_num_to_fail[i] = 0
+            continue
+        stage_num_to_fail[i] = stage_fail_num / total_num
+        total_num -= stage_fail_num
+    sorted_fail = sorted(stage_num_to_fail.items(),key = lambda item: item[0])
+    sorted_fail = sorted(sorted_fail,key = lambda item: item[1],reverse=True)
+    answer = [i[0] for i in sorted_fail]
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution(5,[2, 1, 2, 6, 2, 4, 3, 3]))
