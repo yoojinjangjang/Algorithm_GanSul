471425b24c205cb0e368c608da53a2cafba04f50
wxxhyeong <wxxhyeong@gmail.com>
2022-11-29 21:14:22 +0900

    Dynamic_Programming/11월29일/8-3.py

--- 조우형/Dynamic_Programming/8-3.py
+++ 조우형/Dynamic_Programming/8-3.py
@@ -14,6 +14,11 @@ output
 n = int(input())
 foods = list(map(int, input().split()))
 
+d = [0]*100
+d[0] = foods[0]
+d[1] = max(d[0], foods[1])
 
+for i in range(2, n):
+    d[i] = max(d[i-1], d[i-2]+foods[i])
 
-
+print(d[n-1])
