d91d010cdbbfa744cadf33a17a851c4ddcec1501
wxxhyeong <wxxhyeong@gmail.com>
2022-12-02 00:58:18 +0900

    Sort/12월2일/Q23.py

--- 조우형/Sort/Q23.py
+++ 조우형/Sort/Q23.py
@@ -14,10 +14,8 @@ sys.stdin = open("input.txt", "r")
 n = int(input())
 grade = []
 for _ in range(n):
-    grade.append([input().split()])
+    grade.append(input().split())
 
-print(grade)
-print("-----")
-grade.sort(key=lambda x: (int(x[1]), int(x[2])))
-
-print(grade)
+grade.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+for i in grade:
+    print(i[0])
