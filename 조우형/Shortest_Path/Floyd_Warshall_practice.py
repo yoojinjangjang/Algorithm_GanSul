629a27f35218932abec42dfd1b86d772c3d887cb
wxxhyeong <wxxhyeong@gmail.com>
2022-12-10 15:33:22 +0900

    Shortest_Path/12월10일/floyd_warshall_practice.py(연습)

---
+++ 조우형/Shortest_Path/Floyd_Warshall_practice.py
@@ -0,0 +1,31 @@
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 수행된 결과를 출력
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+        # 도달할 수 있는 경우 거리를 출력
+        else:
+            print(graph[a][b], end = " ")
+    print()
