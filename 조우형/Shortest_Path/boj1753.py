66aa82b00dabfaaf98039c8912b9782acce00f01
wxxhyeong <wxxhyeong@gmail.com>
2022-12-13 15:57:31 +0900

    Shortest_Path/12월13일/boj1753.py(최단경로_수정)

--- 조우형/Shortest_Path/boj1753.py
+++ 조우형/Shortest_Path/boj1753.py
@@ -11,7 +11,7 @@ input = sys.stdin.readline
 
 v, e = map(int, input().split())
 k = int(input())
-graph = [[] for i in range(e+1)]
+graph = [[] for i in range(v+1)]
 INF = int(1e9)
 distance = [INF] * (v+1)
 for i in range(e):
@@ -32,12 +32,12 @@ def dijkstra(start):
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0],))
+                heapq.heappush(q, (cost, i[0]))
 
 
 dijkstra(k)
 
-for d in range(1,n+1):
+for d in range(1, v+1):
     if distance[d] == INF:
         print("INF")
     else:
